22a69b74814bea6a653bf26e88a26aafc325f322
Mubassir-Ali <mubassir11@gmail.com>
2021-04-14 12:46:42 +0500

    add react-twitter-embed

--- src/components/widgets/Widgets.js
+++ src/components/widgets/Widgets.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import './Widgets.css'
+import React from "react";
+import "./Widgets.css";
+import SearchIcon from "@material-ui/icons/Search";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
 
 function Widgets() {
-    return (
-        <div className='widgets'>
-            
-        </div>
-    )
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon />
+        <input type="text" placeholder="Search Twitter" />
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId={"1382237789778014208"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="mubassirali3"
+          options={{ height: 400 }}
+        />
+     <TwitterShareButton
+          url={"https://facebook.com/cleverprogrammer"}
+          options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Widgets
+export default Widgets;
